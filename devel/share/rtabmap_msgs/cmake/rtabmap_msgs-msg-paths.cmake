3c74fd2608a3d6abacc248d9de673dc0d621040a
Vamsi <92263050+Vamsi-IITI@users.noreply.github.com>
2023-03-07 16:48:52 +0530

    Delete devel directory

--- devel/share/rtabmap_msgs/cmake/rtabmap_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(rtabmap_msgs_MSG_INCLUDE_DIRS "/home/raghu/RTAB_Mapping_and_Localization/src/rtabmap_ros/rtabmap_msgs/msg")
-set(rtabmap_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs;sensor_msgs)
